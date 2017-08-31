58cd560334416c2e54f588fc583b0302355fc562
paulo.campos <paulo.campos@basecone.nl>
2017-08-31 16:51:05 +0100

    Project creation

---
+++ StringCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculator.Tests
+{
+    [TestClass]
+    public class StringCalculatorTests
+    {
+        private readonly StringCalculator _calculator = new StringCalculator();
+
+        [TestMethod]
+        public void StringCalculator_InputEmptyString_ReturnZero()
+        {
+            // Arrange
+            var fakeInput = string.Empty;
+            var expectedResult = 0;
+            // Act
+            var result = _calculator.Add(fakeInput);
+            // Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
